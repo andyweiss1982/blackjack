654c6298368f62b7045f19fe9b42582cd5c14f5d
Andy Weiss <andyweiss1982@gmail.com>
2020-01-03 14:28:50 -0500

    Updates

---
+++ precache-manifest.15aa1a7c7c65bae45388e977b5161dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7016fa7ae2d5b6ff881946ce7ab7ce4",
+    "url": "/blackjack/index.html"
+  },
+  {
+    "revision": "da818be6a013dfa81b15",
+    "url": "/blackjack/static/css/main.cc7bc316.chunk.css"
+  },
+  {
+    "revision": "338958ca5c8d11af0c31",
+    "url": "/blackjack/static/js/2.2201d89b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/blackjack/static/js/2.2201d89b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da818be6a013dfa81b15",
+    "url": "/blackjack/static/js/main.45a25711.chunk.js"
+  },
+  {
+    "revision": "52c2e5fedc8be70506d4",
+    "url": "/blackjack/static/js/runtime-main.0772e745.js"
+  }
+]);
