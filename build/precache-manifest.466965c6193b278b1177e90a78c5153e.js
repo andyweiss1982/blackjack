8f7bc73b390f8196e05581976f6c62e5f3f51958
Andy Weiss <andyweiss1982@gmail.com>
2020-01-03 14:21:05 -0500

    build for prod

---
+++ build/precache-manifest.466965c6193b278b1177e90a78c5153e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ade08fa1991a2f282f93e040d9c4e02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffa4a42abb47e802c295",
+    "url": "/static/css/main.cc7bc316.chunk.css"
+  },
+  {
+    "revision": "338958ca5c8d11af0c31",
+    "url": "/static/js/2.2201d89b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2201d89b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffa4a42abb47e802c295",
+    "url": "/static/js/main.97254b9f.chunk.js"
+  },
+  {
+    "revision": "fa550260c0014ca43c13",
+    "url": "/static/js/runtime-main.47de0f37.js"
+  }
+]);
